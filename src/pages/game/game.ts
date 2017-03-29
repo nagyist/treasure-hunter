fcb09f46f493952b2151b821baaaa86a21d95e08
Giannis Potamitis <potamitisgiannis@gmail.com>
2017-03-29 20:33:57 +0300

    game code structure

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import * as PIXI from 'pixi.js';
-import { Howl } from 'howler';
+//import { Howl } from 'howler';
 
 @Component({
   selector: 'page-game',
@@ -16,42 +16,34 @@ export class GamePage {
   constructor(public navCtrl: NavController) {}
 
   ngAfterViewInit() {
+    this.setup();
+  }
+
+  // Initialize the game sprites, set the game `state` to `play` and start the 'gameLoop'
+  setup() {
     // Get the canvas container HTML element
     this.canvasContainer = document.getElementById('pixi-canvas-container');
 
     // Create the app 
     this.app = new PIXI.Application(this.getCanvasWidth(), this.getCanvasHeight(), {backgroundColor : 0xA8AC91});
 
-    // Add the app.view to the HTML document
-    this.canvasContainer.appendChild(this.app.view);
-
     //Set the game's current state to `play`:
     this.state = this.play;
 
-    // TITLE
-    var style = new PIXI.TextStyle({
-        fontFamily: 'Times New Roman',
-        fontSize: 42,
-        fontStyle: 'normal',
-        fontWeight: 'bold',
-        fill: ['#ffffff', '#A45D42'], // gradient
-        stroke: '#A3A78D',
-        strokeThickness: 4,
-        dropShadow: true,
-        dropShadowColor: '#000000',
-        dropShadowBlur: 2,
-        dropShadowAngle: Math.PI / 6,
-        dropShadowDistance: 6,
-        wordWrap: true,
-        wordWrapWidth: 440
+    // Add the app.view to the HTML document
+    this.canvasContainer.appendChild(this.app.view);
+
+    // TITLE SPRITE
+    const title = new PIXI.Text("Treasure Hunter", {
+      fontSize: 48,
+      fill: "#A45D42"
     });
-    const title = new PIXI.Text('Treasure Hunter', style);
     title.anchor.set(0.5);
     title.x = this.app.renderer.width / 2;
     title.y = this.app.renderer.height / 2 - 200;
     this.app.stage.addChild(title);
 
-    // PIRATE
+    // PIRATE SPRITE
     // create a new Sprite from an image path
     const pirate = PIXI.Sprite.fromImage('assets/images/pirate.png')
     
@@ -69,8 +61,8 @@ export class GamePage {
     //Add the sprite to the stage
     this.app.stage.addChild(pirate);
 
-    // Tell the `renderer` to `render` the `stage`
-    var gameLoop = () => {
+    // Runs the current game `state` in a loop and render the stage
+    let gameLoop = () => {
       // Loop this function 60 times per second
       requestAnimationFrame(gameLoop);
 
@@ -85,6 +77,7 @@ export class GamePage {
     gameLoop();
   }
 
+  // All the game logic goes here 
   play() {
     console.log('playing...')
   }
